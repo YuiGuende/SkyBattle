ff97bb940b9e2df94bd3ad14ce92dd452041f62f
YuiGuende <duynguyenrubik968@gmail.com>
2025-06-18 15:38:46 +0700

    update may bay

---
+++ Assets/script/TurnManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+public class TurnManager : MonoBehaviour
+{
+    public static TurnManager Instance;
+
+    public enum PlayerTurn { Player1, Player2 }
+    public PlayerTurn currentTurn = PlayerTurn.Player1;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void EndTurn()
+    {
+        currentTurn = (currentTurn == PlayerTurn.Player1) ? PlayerTurn.Player2 : PlayerTurn.Player1;
+        Debug.Log("Turn chuyển sang: " + currentTurn);
+    }
+
+    public PlayerTurn GetCurrentTurn()
+    {
+        return currentTurn;
+    }
+}
